63751cb880e9249376af0e8c3abb5a4435b23c75
George Véras Valentim <george.veras.valentim@gmail.com>
2019-08-28 16:59:04 -0300

    Update VSCode extensions

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-# from `code --list-extensions`
-modules="
-VisualStudioExptTeam.vscodeintellicode
-vscode-icons-team.vscode-icons
-"
-for module in $modules; do
-  code --install-extension "$module" || true
-done
+code --install-extension dracula-theme.theme-dracula
+code --install-extension Gruntfuggly.todo-tree
+code --install-extension mhutchie.git-graph
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension p1c2u.docker-compose
+code --install-extension ryanluker.vscode-coverage-gutters
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vscode-icons-team.vscode-icons
