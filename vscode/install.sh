c520c517c89022ae2fc6f34c38e0f65ec11f4f33
George Véras Valentim <george.veras.valentim@gmail.com>
2021-07-24 18:05:29 -0300

    Add more VS Code extensions

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,11 +1,33 @@
 #!/usr/bin/env bash
 
+code --install-extension alefragnani.bookmarks
+code --install-extension arjun.swagger-viewer
+code --install-extension dbaeumer.vscode-eslint
 code --install-extension dracula-theme.theme-dracula
+code --install-extension eamodio.gitlens
+code --install-extension felipe.nasc-touchbar
 code --install-extension Gruntfuggly.todo-tree
+code --install-extension hbenl.vscode-mocha-test-adapter
+code --install-extension hbenl.vscode-test-explorer
+code --install-extension jock.svg
+code --install-extension mechatroner.rainbow-csv
 code --install-extension mhutchie.git-graph
 code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-dotnettools.csharp
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode.test-adapter-converter
+code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension msjsdiag.vscode-react-native
+code --install-extension orta.vscode-jest
 code --install-extension p1c2u.docker-compose
+code --install-extension prisma.prisma
+code --install-extension redhat.vscode-yaml
 code --install-extension ryanluker.vscode-coverage-gutters
+code --install-extension scala-lang.scala
+code --install-extension silvenon.mdx
+code --install-extension stephanvs.dot
 code --install-extension VisualStudioExptTeam.vscodeintellicode
 code --install-extension vscode-icons-team.vscode-icons
+code --install-extension waderyan.gitblame
